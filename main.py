af0d9b86b21b11cd5759c8303b1e3011a4498b84
zule <terrazaiturriagozule@gmail.com>
2026-06-19 16:32:12 -0500

    Se agrego biblioteca y cliente

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 mi_app= FastAPI () 
 
 @mi_app.get ("/proyecto")
 
 def mensaje ():
-    return{"proyecto": "este es el proyecto de clientes a desarrollar"}
+    return{"proyecto": "este es el proyecto de clientes a desarrollar.."}
 
 
 lista= ["Yoberson","William","Suns","daniela","kamila","Quiroga"]
 @mi_app.get ("/clientes")
 def Personas ():
     return {"clientes": lista}
+
+class Cliente(BaseModel):
+    id: int
+    nombre: str
+    email: str
+    descripcion: str
